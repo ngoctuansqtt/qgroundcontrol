416442d521e1436d977ad42d33e6018072933ec9
Ngoc-Tuan Do <ngoctuansqtt@gmail.com>
2025-09-01 16:37:38 +0700

    feat: add Vietnamese translation support

---
+++ translations/qgc_source_vi_VN.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi" sourcelanguage="en">
+  <context>
+    <name>ADSBVehicleManager</name>
+    <message>
+      <location filename="../src/ADSB/ADSBVehicleManager.cc" line="70"/>
+      <source>ADSB Server Error: %1</source>
+      <translation>Lỗi máy chủ ADSB: %1</translation>
+    </message>
+  </context>
+</TS>
